2ecc9262520b186d8dfbef3a2ad2e0f9fec81c2e
Anna C <anna.s.chong@gmail.com>
2020-10-19 19:20:20 +1100

    UPDATE navbar styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 // React libraries
 import React from 'react';
+// import logo from '../../public/assets/logo.png';
 
 // Material UI libraries
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
@@ -14,16 +15,17 @@ const Navbar = () => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.background}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            MyMeds
-          </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Signup</Button>
+          <img className={classes.logo} src={process.env.PUBLIC_URL + '/assets/logo.png'} alt='logo' />
+          <div className={classes.title}></div>
+          <div>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Signup</Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
