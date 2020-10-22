2edb429cb2ec6f14363e604dfc2558a9ac6db177
Anna C <anna.s.chong@gmail.com>
2020-10-22 22:29:23 +1100

    EXPORT routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,9 @@
-const router = require("express").Router();
-const userRoutes = require("./user");
+const router = require('express').Router();
+const userRoutes = require('./user');
+const medRoutes = require('./med');
 
 // Routes
-router.use("/user", userRoutes);
+router.use('/user', userRoutes);
+router.use('/med', medRoutes);
 
 module.exports = router;
