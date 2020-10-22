86eea63c55119563f6f04dca436aa7dbe3e43f45
Anna C <anna.s.chong@gmail.com>
2020-10-22 20:14:34 +1100

    UDPATE cleanup

--- backend/routes/api/user/login.js
+++ backend/routes/api/user/login.js
@@ -7,11 +7,10 @@ const User = require('../../../models/user');
 
 router.post('/', passport.authenticate('local'), (req, res) => {
   User.findOne({
-    username: req.body.email
+    username: req.body.username
   }, (err, person) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    console.log(person)
     res.json({
       success: true,
       status: 'You are successfully logged in!',
---
+++ client/src/utils/API.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default {
+  // Gets user
+  login: function (user) {
+    return axios.post('/api/user/login', user);
+  },
+  // Makes new user
+  signup: function (user) {
+    return axios.post('/api/user/signup', user);
+  }
+};
