90be16c382eee91e4a0b1098c25b2187af126e98
Anna C <anna.s.chong@gmail.com>
2020-10-24 09:15:29 +1100

    ADD calendar to home page

---
+++ client/src/pages/Home/calendarIndex.js
@@ -0,0 +1,73 @@
+
+import React, { Component } from 'react';
+import Scheduler, { DemoData, SchedulerData, ViewTypes } from 'react-big-scheduler';
+import 'react-big-scheduler/lib/css/style.css';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+
+class SchedulerComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    //let schedulerData = new SchedulerData(new moment("2017-12-18").format(DATE_FORMAT), ViewTypes.Week);
+    let schedulerData = new SchedulerData('2017-12-18', ViewTypes.Week);
+    schedulerData.localeMoment.locale('en');
+    schedulerData.setResources(DemoData.resources);
+    schedulerData.setEvents(DemoData.events);
+    this.state = {
+      viewModel: schedulerData
+    }
+  }
+
+  prevClick = (schedulerData) => {
+    schedulerData.prev();
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  nextClick = (schedulerData) => {
+    schedulerData.next();
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  onViewChange = (schedulerData, view) => {
+    schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  onSelectDate = (schedulerData, date) => {
+    schedulerData.setDate(date);
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  eventClicked = (schedulerData, event) => {
+    alert(`You just clicked an event: {id: ${event.id}, title: ${event.title}}`);
+  }
+
+  render() {
+    console.log(this.state.viewModel)
+    return (
+      <Scheduler
+        schedulerData={this.state.viewModel}
+        prevClick={this.prevClick}
+        nextClick={this.nextClick}
+        onSelectDate={this.onSelectDate}
+        onViewChange={this.onViewChange}
+        eventItemClick={this.eventClicked}
+      />
+    )
+  }
+}
+export default DragDropContext(HTML5Backend)(SchedulerComponent)
