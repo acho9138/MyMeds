57539d4ca092f893b6ac6b198ce31392ac5cfaaa
Anna C <anna.s.chong@gmail.com>
2020-10-24 09:16:43 +1100

    ADD logout route

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,31 +1,45 @@
 // React library
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
 // Material UI libraries
 import { Paper, Typography, Container } from '@material-ui/core';
-
 // Components
 import { Navbar } from '../../components';
-
 // Custom styles
 import { styles } from './Home.style';
-
 // utils
-// import API from '../../utils/API';
+import API from '../../utils/API';
+// Calendar API
+import Scheduler from './calendarIndex'
 
-const Home = () => {
+
+// Home component
+const Home = (props) => {
   const classes = styles();
+  const history = useHistory();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    API.logout()
+      .then(() => {
+        history.push('/');
+        console.log('Successfully logged out');
+      }).catch((error) => {
+        console.error(error);
+      })
+  };
 
   return (
     <>
-      <Navbar isLoggedIn={true} />
+      <Navbar onClick={handleLogout} />
       <Container maxWidth='sm' className={classes.container}>
         <Paper elevation={3} className={classes.paper}>
-          <Typography variant='h1' gutterBottom>
-            Hello
+          <Typography variant='h3' gutterBottom>
+            Welcome Back
           </Typography>
         </Paper>
       </Container>
+      <Scheduler />
     </>
   )
 }
