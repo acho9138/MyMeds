ff3229ce80a9bf71132adcf73c1f5710db004da5
Anna C <anna.s.chong@gmail.com>
2020-10-26 18:44:15 +1100

    ADD navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,69 @@
 // React libraries
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 // Pages
 import { Login, Signup, Home } from './pages';
 
+// Components
+import { Navbar } from './components';
+
 const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('userId') ? true : false);
   return (
     <>
       <Router basename='/'>
+        <Navbar setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
         <Switch>
-          <Route exact path='/login'><Login /></Route>
-          <Route exact path='/signup'><Signup /></Route>
-          <Route exact path='/home'><Home /></Route>
+          <NormalRoute isLoggedIn={isLoggedIn} exact path='/login'><Login setIsLoggedIn={setIsLoggedIn} /></NormalRoute>
+          <NormalRoute isLoggedIn={isLoggedIn} exact path='/signup'><Signup setIsLoggedIn={setIsLoggedIn} /></NormalRoute>
+          <PrivateRoute isLoggedIn={isLoggedIn} exact path='/home'><Home /></PrivateRoute>
+          <Route path="*"><h1>404: Page Not Found</h1></Route>
         </Switch>
       </Router>
     </>
+  );
+}
+
+const PrivateRoute = ({ children, isLoggedIn, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isLoggedIn ? (
+          children
+        ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+      }
+    />
+  );
+}
+
+
+const NormalRoute = ({ children, isLoggedIn, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isLoggedIn ? (
+          <Redirect
+            to={{
+              pathname: "/home",
+              state: { from: location }
+            }}
+          />
+        ) : (
+            children
+          )
+      }
+    />
   );
 }
 
