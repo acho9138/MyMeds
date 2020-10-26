9ca91fe36ebbc2a7e3bc969389cecfc5f649994d
Anna C <anna.s.chong@gmail.com>
2020-10-26 18:37:17 +1100

    ADD functionality to add med to db

--- client/src/components/MedForm/MedForm.jsx
+++ client/src/components/MedForm/MedForm.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 // Material UI libraries
-import { Modal, Fade, Backdrop, Typography, TextField, MenuItem, Button } from '@material-ui/core';
+import { Modal, Fade, Backdrop, Typography, TextField, MenuItem, Button, FormControl } from '@material-ui/core';
 
 // Material UI calendar imports
 import DateFnsUtils from '@date-io/date-fns';
@@ -14,6 +14,9 @@ import {
 
 // Styles
 import { styles } from './MedForm.style';
+
+// utils
+import API from '../../utils/API';
 
 const frequencies = [
   {
@@ -33,11 +36,11 @@ const frequencies = [
     label: 'FOUR times daily',
   },
   {
-    value: '1xweek',
+    value: '1xweekly',
     label: 'ONCE a WEEK',
   },
   {
-    value: '1xmonth',
+    value: '1xmonthly',
     label: 'ONCE a MONTH',
   },
 ];
@@ -45,113 +48,128 @@ const frequencies = [
 // Component
 const MedForm = (props) => {
   const classes = styles();
-  const [frequency, setFrequency] = useState();
-  const [selectedTime, setselectedTime] = useState();
+  const [name, setName] = useState('');
+  const [strength, setStrength] = useState('');
+  const [frequency, setFrequency] = useState('');
+  const [selectedTime, setselectedTime] = useState(new Date());
   const [selectedStartDate, setselectedStartDate] = useState(new Date());
   const [selectedEndDate, setSelectedEndDate] = useState(null);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    API.addMed({
+      userId: localStorage.getItem('userId'),
+      name: name,
+      strength: strength,
+      frequency: frequency,
+      time: selectedTime,
+      startDate: selectedStartDate,
+      endDate: selectedEndDate
+    }).then(() => console.log('Successfully added new medication')
+    ).catch((error) => {
+      console.error(error);
+    })
+  }
+
   return (
-    <>
-      <Modal
-        aria-labelledby='transition-modal-title'
-        aria-describedby='transition-modal-description'
-        className={classes.modal}
-        open={props.open}
-        onClose={props.onClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={props.in}>
-          <div className={classes.paper}>
-            <Typography variant='h5' gutterBottom>
-              Add a Medication
+    <Modal
+      aria-labelledby='transition-modal-title'
+      aria-describedby='transition-modal-description'
+      className={classes.modal}
+      open={props.open}
+      onClose={props.onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={props.in}>
+        <div className={classes.paper}>
+          <Typography variant='h5' gutterBottom>
+            Add a Medication
             </Typography>
-            <form className={classes.root}>
-              <div>
-                <TextField
-                  label='Medication name'
-                  id='outlined-size-small'
-                  variant='outlined'
+          <FormControl className={classes.root}>
+            <div>
+              <TextField
+                label='Medication name'
+                variant='outlined'
+                size='small'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                label='Strength'
+                variant='outlined'
+                size='small'
+                value={strength}
+                onChange={(e) => setStrength(e.target.value)}
+              />
+            </div>
+            <div>
+              <TextField
+                select
+                label='Select frequency'
+                value={frequency}
+                onChange={(event) => setFrequency(event.target.value)}
+                variant='outlined'
+                size='small'
+              >
+                {frequencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardTimePicker
                   size='small'
+                  margin='normal'
+                  id='time-picker'
+                  label='Set time of dose'
+                  value={selectedTime}
+                  onChange={(time) => setselectedTime(time)}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                  }}
                 />
-                <TextField
-                  label='Strength'
-                  id='outlined-size-small'
-                  variant='outlined'
-                  size='small'
+              </MuiPickersUtilsProvider>
+            </div>
+            <div>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant='inline'
+                  format='dd/MM/yyyy'
+                  margin='normal'
+                  label='Start date'
+                  value={selectedStartDate}
+                  onChange={(date) => setselectedStartDate(date)}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
                 />
-              </div>
-              <div>
-                <TextField
-                  id='outlined-select-currency'
-                  select
-                  label='Select frequency'
-                  value={frequency}
-                  onChange={(event) => setFrequency(event.target.value)}
-                  variant='outlined'
-                  size='small'
-                >
-                  {frequencies.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  <KeyboardTimePicker
-                    size='small'
-                    margin='normal'
-                    id='time-picker'
-                    label='Set time of dose'
-                    value={selectedTime}
-                    onChange={(time) => setselectedTime(time)}
-                    KeyboardButtonProps={{
-                      'aria-label': 'change time',
-                    }}
-                  />
-                </MuiPickersUtilsProvider>
-              </div>
-              <div>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  <KeyboardDatePicker
-                    disableToolbar
-                    variant='inline'
-                    format='dd/MM/yyyy'
-                    margin='normal'
-                    id='date-picker-inline'
-                    label='Start date'
-                    value={selectedStartDate}
-                    onChange={(date) => setselectedStartDate(date)}
-                    KeyboardButtonProps={{
-                      'aria-label': 'change date',
-                    }}
-                  />
-                  <KeyboardDatePicker
-                    disableToolbar
-                    variant='inline'
-                    format='dd/MM/yyyy'
-                    margin='normal'
-                    id='date-picker-inline'
-                    label='End date (OPTIONAL)'
-                    value={selectedEndDate}
-                    onChange={(date) => setSelectedEndDate(date)}
-                    KeyboardButtonProps={{
-                      'aria-label': 'change date',
-                    }}
-                  />
-                </MuiPickersUtilsProvider>
-              </div>
-              <Button className={classes.addButton} onClick={props.onClick} variant="contained" color="primary">
-                Add
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant='inline'
+                  format='dd/MM/yyyy'
+                  margin='normal'
+                  label='End date (OPTIONAL)'
+                  value={selectedEndDate}
+                  onChange={(date) => setSelectedEndDate(date)}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </div>
+            <Button className={classes.addButton} onClick={handleSubmit} variant="contained" color="primary">
+              Add
               </Button>
-            </form>
-          </div>
-        </Fade>
-      </Modal>
-    </>
+          </FormControl>
+        </div>
+      </Fade>
+    </Modal>
   );
 }
 
