b77cea4c03c9ac285c6deffafd038c998d391e96
Anna C <anna.s.chong@gmail.com>
2020-10-26 18:44:03 +1100

    UPDATE navbar to app.js

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,46 +1,28 @@
 // React library
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 // Material UI libraries
 import { Paper, Typography, Container, Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 // Components
-import { Navbar } from '../../components';
 import Medform from '../../components/MedForm/MedForm';
 
 // Custom styles
 import { styles } from './Home.style';
 
-// utils
-import API from '../../utils/API';
-
 // Calendar API
-import Scheduler from './calendarIndex'
+import { Calendar } from './_components'
 
 
 // Home component
 const Home = (props) => {
   const classes = styles();
-  const history = useHistory();
 
   const [open, setOpen] = useState(false);
 
-  const handleLogout = (e) => {
-    e.preventDefault();
-    API.logout()
-      .then(() => {
-        history.push('/');
-        console.log('Successfully logged out');
-      }).catch((error) => {
-        console.error(error);
-      })
-  };
-
   return (
     <>
-      <Navbar onClick={handleLogout} />
       <Container maxWidth='sm' className={classes.container}>
         <Paper elevation={3} className={classes.paper}>
           <Typography variant='h3' gutterBottom>
@@ -48,7 +30,7 @@ const Home = (props) => {
           </Typography>
         </Paper>
       </Container>
-      <Scheduler />
+      <Calendar />
       <Fab className={classes.button} onClick={() => setOpen(true)} color='primary' aria-label='add'>
         <AddIcon />
       </Fab>
