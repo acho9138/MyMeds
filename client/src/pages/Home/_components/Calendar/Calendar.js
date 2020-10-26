a9a3b267e3dccbc310a9983c90947d29b10c445f
Anna C <anna.s.chong@gmail.com>
2020-10-26 21:45:46 +1100

    ADD meds to calendar from db

--- client/src/pages/Home/_components/Calendar/Calendar.js
+++ client/src/pages/Home/_components/Calendar/Calendar.js
@@ -2,11 +2,11 @@
 import React, { Component } from 'react';
 
 // React Big Calendar
-import Scheduler, { DemoData, SchedulerData, ViewTypes } from 'react-big-scheduler';
+import Scheduler, { SchedulerData, ViewTypes } from 'react-big-scheduler';
 import 'react-big-scheduler/lib/css/style.css';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import moment from 'moment'
+import moment from 'moment';
 
 // utils
 import API from '../../../../utils/API';
@@ -19,10 +19,11 @@ class Calendar extends Component {
     //let schedulerData = new SchedulerData(new moment("2017-12-18").format(DATE_FORMAT), ViewTypes.Week);
     let schedulerData = new SchedulerData(moment().format('YYYY-MM-DD'), ViewTypes.Week);
     schedulerData.localeMoment.locale('en');
-    schedulerData.setResources(DemoData.resources);
-    schedulerData.setEvents(DemoData.events);
+
     this.state = {
-      viewModel: schedulerData
+      viewModel: schedulerData,
+      resources: [],
+      events: []
     }
   }
 
@@ -39,18 +40,50 @@ class Calendar extends Component {
 
   getEvents(data) {
     let events = []
+    const colors = ['#7fc5f6', '#f759ab', '#51adcf', '#B56AD7', '#7579e7', '#93b5e1', '#f6acc8'];
+
     for (let i = 0; i < data.length; i++) {
-      const med = data[i]
-      const time = new Date(med.startDate)
-      console.log(time.getTime())
-      events.push({
-        id: i + 1,
-        start: moment(med.startDate).format('YYYY-MM-DD hh:mm:ss'),
-        end: moment(med.startDate).add(15, 'minutes').format('YYYY-MM-DD hh:mm:ss'),
-        resourceId: med.name,
-        movable: false,
-        title: 'Take pills sucker',
-      })
+      const med = data[i];
+      const time = moment(med.startDate).format('h:mm A');
+      const recurringDay = moment(med.startDate).format('dd').toUpperCase();
+      const recurringDate = moment(med.startDate).format('D');
+      const recurringStartDay = med.startDate.replaceAll('-', '').replaceAll(':', '').replaceAll('.', '').slice(0, -4);
+
+      if (med.frequency === '1xdaily') {
+        events.push({
+          id: i + 1,
+          start: moment(med.startDate).format('YYYY-MM-DD hh:mm:ss'),
+          end: moment(med.startDate).add(5, 'minutes').format('YYYY-MM-DD hh:mm:ss'),
+          resourceId: med.name,
+          movable: false,
+          bgColor: colors[i],
+          title: `${med.name} ${time}`,
+          rrule: `FREQ=DAILY;DTSTART=${recurringStartDay};BYDAY=MO,TU,WE,TH,FR,SA,SU`,
+        })
+      } else if (med.frequency === '1xmonthly') {
+        events.push({
+          id: i + 1,
+          start: moment(med.startDate).format('YYYY-MM-DD hh:mm:ss'),
+          end: moment(med.startDate).add(5, 'minutes').format('YYYY-MM-DD hh:mm:ss'),
+          resourceId: med.name,
+          movable: false,
+          bgColor: colors[i],
+          title: `${med.name} ${time}`,
+          rrule: `FREQ=MONTHLY;DTSTART=${recurringStartDay};BYMONTHDAY=${recurringDate}`,
+        })
+      } else if (med.frequency === '1xweekly') {
+        events.push({
+          id: i + 1,
+          start: moment(med.startDate).format('YYYY-MM-DD hh:mm:ss'),
+          end: moment(med.startDate).add(5, 'minutes').format('YYYY-MM-DD hh:mm:ss'),
+          resourceId: med.name,
+          movable: false,
+          bgColor: colors[i],
+          title: `${med.name} ${time}`,
+          rrule: `FREQ=WEEKLY;DTSTART=${recurringStartDay};BYDAY=${recurringDay}`,
+        })
+      }
+
     }
     return events;
   }
@@ -58,12 +91,15 @@ class Calendar extends Component {
   componentDidMount() {
     API.getMeds(localStorage.getItem('userId'))
       .then((res) => {
-        console.log(res.data)
         let schedulerData = this.state.viewModel
         schedulerData.setResources(this.getResources(res.data))
         schedulerData.setEvents(this.getEvents(res.data))
 
-        this.setState({ viewModel: schedulerData })
+        this.setState({
+          viewModel: schedulerData,
+          resources: this.getResources(res.data),
+          events: this.getEvents(res.data)
+        })
       }).catch((error) => {
         console.error(error)
       })
@@ -71,33 +107,33 @@ class Calendar extends Component {
 
   prevClick = (schedulerData) => {
     schedulerData.prev();
-    schedulerData.setEvents(DemoData.events);
+    schedulerData.setEvents(this.state.events);
     this.setState({
-      viewModel: schedulerData
+      viewModel: schedulerData,
     })
   }
 
   nextClick = (schedulerData) => {
     schedulerData.next();
-    schedulerData.setEvents(DemoData.events);
+    schedulerData.setEvents(this.state.events);
     this.setState({
-      viewModel: schedulerData
+      viewModel: schedulerData,
     })
   }
 
   onViewChange = (schedulerData, view) => {
     schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
-    schedulerData.setEvents(DemoData.events);
+    schedulerData.setEvents(this.state.events);
     this.setState({
-      viewModel: schedulerData
+      viewModel: schedulerData,
     })
   }
 
   onSelectDate = (schedulerData, date) => {
     schedulerData.setDate(date);
-    schedulerData.setEvents(DemoData.events);
+    schedulerData.setEvents(this.state.events);
     this.setState({
-      viewModel: schedulerData
+      viewModel: schedulerData,
     })
   }
 
