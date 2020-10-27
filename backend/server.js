8d9af05eacf9ccb5d1b36ee289a66e0f482c04db
Anna C <anna.s.chong@gmail.com>
2020-10-27 21:58:48 +1100

    UPDATE route for heroku

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,9 @@ mongoose.connect(
 //   Reminder.medReminder()
 // });
 
-app.get('*', res => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+);
 
 // Start the API server
 app.listen(PORT, () => {
