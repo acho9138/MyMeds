ff52d5dec018b6d7cf4e9b29347b62a380f767b1
Anna C <anna.s.chong@gmail.com>
2020-10-27 22:38:36 +1100

    UPDATE path to client file

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
 }
 
 app.use(session({
@@ -64,7 +64,7 @@ mongoose.connect(
 // });
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + '../client/build/index.html'))
 );
 
 // Start the API server
