2c9003f743c055ca0c1f0c27338f6986250510f1
Anna C <anna.s.chong@gmail.com>
2020-10-27 14:03:15 +1100

    UPDATE to arrow function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,26 +2,26 @@ import axios from 'axios';
 
 export default {
   // Gets user
-  login: function (user) {
+  login: (user) => {
     return axios.post('/api/user/login', user);
   },
   // Makes new user
-  signup: function (user) {
+  signup: (user) => {
     return axios.post('/api/user/signup', user);
   },
-  logout: function () {
+  logout: () => {
     return axios.get('/api/user/logout');
   },
-  getMeds: function (userId) {
+  getMeds: (userId) => {
     return axios.get('/api/med/' + userId);
   },
-  addMed: function (med) {
+  addMed: (med) => {
     return axios.post('/api/med', med);
   },
-  editMed: function (medId) {
-    return axios.put(`/api/med/${medId}`);
+  editMed: (medId, res) => {
+    return axios.put(`/api/med/${medId}`, res);
   },
-  deleteMed: function (medId) {
+  deleteMed: (medId) => {
     return axios.delete(`/api/med/${medId}`);
   }
 };
