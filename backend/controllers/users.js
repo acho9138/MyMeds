cc279bea32854a5e783b09bb6bfe1fdfd8065bf0
Anna C <anna.s.chong@gmail.com>
2020-10-27 20:03:15 +1100

    UPDATE signup function

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -36,29 +36,26 @@ module.exports = {
     }
   },
   signup: function (req, res, next) {
-    UserDB.register(new User({
+    console.log(req.body.username)
+    UserDB.register(new UserDB({
       username: req.body.username
     }),
       req.body.password, (err, user) => {
         if (err) {
+          console.log(err.message)
+          console.log("ERROR")
           res.statusCode = 500;
           res.setHeader('Content-Type', 'application/json');
           res.json({
             err: err
           });
         } else {
-          passport.authenticate('local')(req, res, () => {
-            UserDB.findOne({
-              username: req.body.username
-            }, (err, person) => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json({
-                success: true,
-                status: 'Registration Successful!',
-              });
-            });
-          })
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({
+            success: true,
+            message: 'Registration Successful!',
+          });
         }
       })
   }
