e4d23179b44cc0f90fab39540346765169cd994b
Anna C <anna.s.chong@gmail.com>
2020-11-01 09:00:42 +1100

    UPDATE serve static assets for heroku

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-//if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'));
-//}
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use(session({
   name: 'session-id',
