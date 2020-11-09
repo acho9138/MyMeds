ead7edb05762d9234264661f69d6f1ca3c3c2fc0
Anna C <anna.s.chong@gmail.com>
2020-11-09 22:53:19 +1100

    ADD table styling

---
+++ client/src/pages/Summary/Summary.style.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const styles = makeStyles((theme) => ({
+  container: {
+    paddingTop: '120px'
+  },
+  paper: {
+    textAlign: 'center',
+    marginBottom: '50px',
+  }
+})
+)
