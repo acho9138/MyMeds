dae4d3c9d12fe4db8c6582cc683d63b285a30fb7
Anna C <anna.s.chong@gmail.com>
2020-11-10 19:41:14 +1100

    ADD sidenav styles

---
+++ client/src/components/SideNav/SideNav.style.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const styles = makeStyles((theme) => ({
+  drawer: {
+    paddingRight: '100px',
+    paddingTop: '30px',
+    width: '100%',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  }
+}));
