43ff3e0b132d566ca82bc09c20cfaf4d23c43f1b
Anna C <anna.s.chong@gmail.com>
2020-11-10 17:47:27 +1100

    UPDATE table size

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,53 +1,29 @@
+// React library
 import * as React from 'react';
+
+// Material Ui components
 import { DataGrid } from '@material-ui/data-grid';
-import {
-  randomCreatedDate,
-  randomUpdatedDate,
-} from '@material-ui/x-grid-data-generator';
 
-const rows = [
-  {
-    id: 1,
-    Medication: 'Damien',
-    Strength: '25',
-    Frequency: 'ONCE daily',
-    Time: '9am',
-    StartDate: randomCreatedDate(),
-    EndDate: randomUpdatedDate(),
-  },
-  {
-    id: 2,
-    Medication: 'Nicolas',
-    Strength: '36',
-    Frequency: 'ONCE daily',
-    Time: '9am',
-    StartDate: randomCreatedDate(),
-    EndDate: randomUpdatedDate(),
-  },
-  {
-    id: 3,
-    Medication: 'Kate',
-    Strength: '10mg',
-    Frequency: 'ONCE daily',
-    Time: '9am',
-    StartDate: randomCreatedDate(),
-    EndDate: randomUpdatedDate(),
-  },
-];
+// Custom styles
+import { styles } from './Table.style';
 
-const Table = () => {
+
+// Component
+const Table = (props) => {
+  const classes = styles();
+
   return (
-    <div style={{ height: 300, width: '100%' }}>
+    <div className={classes.root}>
       <DataGrid
         columns={[
-          { field: 'Medication', type: 'string', width: 130 },
-          { field: 'Strength', type: 'string', width: 130 },
-          { field: 'Frequency', type: 'string', width: 130 },
-          { field: 'Time', type: 'string', width: 130 },
-          { field: 'StartDate', type: 'dateTime', width: 180 },
-          { field: 'EndDate', type: 'dateTime', width: 180 },
+          { field: 'Medication', type: 'string', width: 150 },
+          { field: 'Strength', type: 'string', width: 150 },
+          { field: 'Frequency', type: 'string', width: 170 },
+          { field: 'Time', type: 'string', width: 150 },
+          { field: 'StartDate', type: 'dateTime', width: 170 },
+          { field: 'EndDate', type: 'dateTime', width: 170 },
         ]}
-        rows={rows}
+        rows={props.rows}
       />
     </div>
   );
