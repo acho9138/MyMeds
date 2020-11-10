137dbcacde50bd82d2e60f09d175a768c490df43
Anna C <anna.s.chong@gmail.com>
2020-11-10 17:46:00 +1100

    ADD table styling

---
+++ client/src/components/Table/Table.style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const styles = makeStyles((theme) => ({
+  root: {
+    height: 300,
+    width: '80%',
+    margin: 'auto'
+  }
+}));
