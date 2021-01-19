de3342271058be98045a0c887271803890826b41
Anna C <anna.s.chong@gmail.com>
2021-01-19 14:08:21 +1100

    UPDATE table width

--- client/src/components/Table/Table.style.js
+++ client/src/components/Table/Table.style.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const styles = makeStyles((theme) => ({
   root: {
     height: 300,
-    width: '80%',
+    width: '65%',
     margin: 'auto'
   }
 }));
